e8546db0ca408704b0bd5eb5a6f24e50b45b56ba
okdistlab <106539138+okdistlab@users.noreply.github.com>
2022-05-30 16:46:13 +0300

    Update Practice1.py
    Fixed version

--- Practice1.py
+++ Practice1.py
@@ -2,29 +2,17 @@ import random
 import time
 
 for i in range(3,13):
-    bit_s = 2**i                                  #біти
-    keys = bit_s**i                               #кількість варіантів ключів
-    for k in range(1,keys+1):
-        rand_key = int(random.getrandbits(bit_s)) #випадкове значення ключа,що залежить від довжини ключа
-        def key_mean():
-            start = time.monotonic_ns()
-            print(start)
-            k = 0
-            while k != rand_key:
-                k += 1
-                continue
+    bit_s = 2**i                                  # біти
+    keys = 2**bit_s                               # кількість варіантів ключів
+    rand_key = int(random.getrandbits(bit_s))     # випадкове значення ключа,що залежить від довжини ключа
+    print(rand_key)
+    start = time.monotonic_ns()
+    k = 0
+    for k in range(0,keys):
+        if k == rand_key:
             print(k)
-            finish = time.monotonic_ns()
-            print(finish)
-            duration = finish - start
-            print(f"That took {duration // 1000000}ms")   #кількість часу в мілісекундах,яка була витрачена
+            break
+    finish = time.monotonic_ns()
+    duration = finish - start
+    print(f"That took {duration // 1000000}ms")   #кількість часу в мілісекундах,яка була витрачена
                                                           #для знаходження ключа
-        key_mean()
-
-
-
-
-
-
-
-
